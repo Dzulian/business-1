b83478857fca5948e73bdb4c9a639bc2500011ea
Kinga Izdebska <kinga.izdebska@gmail.com>
2021-02-24 19:56:34 +0100

    Pierwszy commint

--- src/pl/sdaacademy/podstawy/Company.java
+++ src/pl/sdaacademy/podstawy/Company.java
@@ -1,12 +1,32 @@
 package pl.sdaacademy.podstawy;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Arrays;
+
 public class Company {
     private Country country;
     private String companyName;
+    private Employee[] employees;
+
+    public Employee[] getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Employee[] employees) {
+        if ( employees == null ) {
+            this.employees = new Employee[0];
+        }
+        else {
+            this.employees = employees;
+        }
+
+    }
 
     public Company(Country country, String companyName) {
         this.country = country;
         this.companyName = companyName;
+        this.employees = new Employee[0];
     }
 
     public Country getCountry() {
@@ -19,5 +39,18 @@ public class Company {
 
     public String toString() {
         return companyName;
+    }
+
+    public Employee[] getRetiredEmployees(LocalDate date){
+        Employee[] retiredEmployees = new Employee[employees.length];
+        int counter = 0;
+        for (Employee employee: this.employees) {
+           if (employee.isRetired(getCountry(),date)) {
+               retiredEmployees[counter]=employee;
+               counter++;
+           }
+        }
+        return Arrays.copyOf(retiredEmployees,counter);
+
     }
 }
--- src/pl/sdaacademy/podstawy/Main.java
+++ src/pl/sdaacademy/podstawy/Main.java
@@ -1,14 +1,40 @@
 package pl.sdaacademy.podstawy;
 
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
         Country countryPoland = new Country(65, "Poland");
-        Country countryGermany = new Country(65, "Poland");
+        Country countryGermany = new Country(65, "Germany");
 
         for( Country c: new Country[]{countryPoland, countryGermany})
         {
             System.out.println(c);
+        }
+
+        Company terravita = new Company(countryPoland,"Terravita");
+        Company volkswagen = new Company(countryGermany,"Volkswagen");
+
+        Employee employee1 = new Employee("Hans","Schulz", LocalDate.of(1970,02,10));
+        Employee employee2 = new Employee("Andreas","Schulz22", LocalDate.of(2001,02,10));
+        terravita.setEmployees(null);
+        volkswagen.setEmployees(new Employee[]{employee1,employee2});
+        terravita.setEmployees(new Employee[]{
+                new Employee("Anna","Bania", LocalDate.of(2001,02,10)),
+                new Employee("Hanna","Bania", LocalDate.of(1967,02,10))
+        });
+
+
+        for (Company company: new Company[]{terravita,volkswagen}) {
+            for (Employee employee: company.getEmployees()) {
+                System.out.printf("Nazwa firmy: %s, pracownik: %s, kraj: %s\n", company.getCompanyName(), employee, company.getCountry());
+            }
+        }
+
+        for(Employee employee: volkswagen.getRetiredEmployees(LocalDate.of(2040,3,4)))
+        {
+            System.out.println(employee);
         }
     }
 
